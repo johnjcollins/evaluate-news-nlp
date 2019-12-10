1132eee19d8d353673f559ee3e08a5a266793876
John J. Collins <johnj_collins@charter.net>
2019-12-10 18:24:17 -0500

    feat: Webpack configuration complete

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,37 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
+const json = {
+  title: 'test json response',
+  message: 'this is a message',
+  time: 'now'
+};
 
-app.use(express.static('src/client'))
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-console.log(__dirname)
+app.use(express.static('dist'));
 
-app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
-})
+console.log(JSON.stringify(mockAPIResponse));
+
+app.get('/', function(req, res) {
+  res.sendFile('dist/index.html');
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(8081, function() {
+  console.log('Example app listening on port 8081!');
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get('/test', function(req, res) {
+  res.send(mockAPIResponse);
+});
